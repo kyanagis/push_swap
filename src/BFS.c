7f818cf765f9c877375945d5ef31f534110618b6
kyanagis <kyanagis42@gmail.com>
2025-07-29 23:15:29 +0900

    push:algo

---
+++ src/BFS.c
@@ -0,0 +1,200 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_NODES 100
+
+typedef struct 
+{
+    int items[MAX_NODES];
+    int front;
+    int rear;
+}Queue;
+
+Queue* createQueue()
+{
+    Queue* q = (Queue*)malloc(sizeof(Queue));
+    q->front = -1;
+    q->rear = -1;
+    return q;
+}
+int isEmpty(Queue* q)
+{
+    return q->rear == -1;
+}
+
+void enqueue(Queue* q,int value)
+{
+    if(q->rear == MAX_NODES -1)
+        printf("Queue is full\n");
+    else{
+        if(q->front == -1)
+            q->front = 0;
+        q->rear++;
+        q->items[q->rear] = value;
+    }
+
+}
+int dequeue(Queue *q)
+{
+    int item;
+    if(isEmpty(q))
+    {
+        printf("Queue is empty\n");
+        item = -1;
+    }else{
+        item = q->items[q->front];
+        q->front++;
+    }
+    if(q->front > q->rear){
+        q->front = q->rear = -1;
+        q->rear = -1;
+    }
+        return item;
+}
+
+typedef struct  Node
+{
+    int vertex;
+    struct  Node* next;
+}Node;
+
+typedef struct {
+    int numVertices;
+    Node** adjLists;
+    int* visited;
+} Graph;
+
+Node *createNode(int v)
+{
+    Node *newNode = malloc(sizeof(Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+Graph *createGraph(int vertices)
+{
+    int i = 0;
+    Graph *graph = malloc(sizeof(Graph));
+    graph->numVertices = vertices;
+
+    graph->adjLists = malloc(vertices * sizeof(Node*));
+    graph->visited = malloc(vertices * sizeof(int));
+
+    while(i < vertices)
+    {
+        graph->adjLists[i] = NULL;
+        graph->visited[i] = 0;
+        ++i;
+    }
+    return graph;
+}
+
+
+void addEdge(Graph *graph,int src,int dest)
+{
+    Node *newNode = createNode(dest);
+    newNode->next = graph->adjLists[src];
+    graph->adjLists[src] = newNode;
+
+    newNode = createNode(src);
+    newNode->next = graph->adjLists[dest];
+    graph->adjLists[dest] = newNode;
+}
+
+
+
+void bfs(Graph *graph,int startVertex)
+{
+    Queue *q = createQueue();
+
+    graph->visited[startVertex] = 1;
+    enqueue(q,startVertex);
+
+    while(!isEmpty(q))
+    {
+        int currentVertex = dequeue(q);
+        printf("Visited %d\n",currentVertex);
+
+        Node *temp = graph->adjLists[currentVertex];
+        while(temp)
+        {
+            int adjVertex = temp->vertex;
+
+            if(graph->visited[adjVertex] == 0)
+            {
+                graph->visited[adjVertex] = 1;
+                enqueue(q,adjVertex);
+            }
+            temp = temp->next;
+        }
+
+
+    }
+    free(q);
+}
+
+
+
+
+
+
+
+
+
+
+void printGraph(Graph* graph) {
+    for (int v = 0; v < graph->numVertices; v++) {
+        Node* temp = graph->adjLists[v];
+        printf("\n Adjacency list of vertex %d\n ", v);
+        while (temp) {
+            printf("%d -> ", temp->vertex);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+void freeGraph(Graph* graph) {
+    if (graph) {
+        if (graph->adjLists) {
+            for (int i = 0; i < graph->numVertices; i++) {
+                Node* current = graph->adjLists[i];
+                while (current != NULL) {
+                    Node* temp = current;
+                    current = current->next;
+                    free(temp);
+                }
+            }
+            free(graph->adjLists);
+        }
+        if (graph->visited) {
+            free(graph->visited);
+        }
+        free(graph);
+    }
+}
+
+
+
+int main() {
+    Graph* graph = createGraph(6);
+
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 2);
+    addEdge(graph, 1, 3);
+    addEdge(graph, 2, 4);
+    addEdge(graph, 3, 4);
+    addEdge(graph, 3, 5);
+    addEdge(graph, 4, 5);
+
+    printGraph(graph);
+
+    printf("\nBreadth First Traversal (starting from vertex 0):\n");
+    bfs(graph, 0);
+
+    freeGraph(graph);
+
+    return 0;
+}
+
+
--- src/push_swap.c
+++ src/push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kyanagis <kyanagis@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: kyanagis <kyanagis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 05:51:04 by kyanagis          #+#    #+#             */
-/*   Updated: 2025/07/24 17:59:38 by kyanagis         ###   ########.fr       */
+/*   Updated: 2025/07/29 23:14:43 by kyanagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,127 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void	error_exit(void)
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+
+typedef struct  s_node
 {
-	write(STDERR_FILENO, "Error\n", 6);
-	exit(EXIT_FAILURE);
+    int value;
+    struct s_node *prev;
+    struct s_node *next;
+}t_node;
+
+void error_exit()
+{
+    write(STDERR_FILENO,"Error\n",6);
+    exit(EXIT_FAILURE);
 }
 
-int	main(int ac, char **av)
+
+t_node *create_new_node(int value)
 {
-	int len = ac - 1;
-	int *stack = malloc(sizeof(int) * len);
-	if (!stack)
-		error_exit();
+    t_node *new_node;
+    new_node = malloc(sizeof(t_node));
+    if(!new_node)
+        error_exit();
+    new_node->value = value;
+    new_node->prev = NULL;
+    new_node->next = NULL;
+    return new_node;
 }
+
+
+void append_node(t_node **stack,int num)
+{
+    t_node *new_node;
+    t_node *last;
+    new_node = create_new_node(num);
+    if(!*stack)
+    {
+        *stack = new_node;
+        return ;
+    }
+    last = *stack;
+    while(last->next)
+        last = last->next;
+
+        last->next = new_node;
+        new_node->prev = last;
+}
+
+
+int ft_isdigit(int c)
+{
+    return(c >= '0' && c <= '9');
+}
+int is_valid_number(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (s[0] == '+' || s[0] == '-')
+		i = 1;
+	while (s[i])
+	{
+		if (s[i] != '.' && !ft_isdigit(s[i]))
+			return (0);
+		++i;
+	}
+	if (i == 0 || (i == 1 && (s[0] == '+' || s[0] == '-')))
+		return (0);
+	return (1);
+}
+
+void parse_arguments(int argc,char **argv,t_node **stack_a)
+{
+    int i;
+    long num;
+
+    if(argc == 2)
+    {
+        //splitを使う
+    }
+    i = 1;
+    while(i < argc)
+    {
+        if(!is_valid_number(argv[i]))
+            error_exit();
+        num = ft_atol(argv[i]);
+        if(num > INT_MAX || num < INT_MIN)
+            error_exit();
+            append_node(stack_a,(int)num);
+            ++i;
+    }
+
+    //追加したスタックに重複がないかの処理
+}
+
+int main(int argc,char **argv)
+{
+t_node *stack_a = NULL;
+
+    if(argc < 2)
+        return 0;
+
+parse_arguments(argc, argv, &stack_a);
+
+
+
+//sort algo　追加予定
+
+	t_node *current = stack_a;
+	t_node *next;
+	while (current)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+
+	return (EXIT_SUCCESS);
+}
+
+
+
